25ac45aa9776fa5ef7c674b6ea457468dd8e00e7
Arye <dabergshoeff@gmail.com>
2020-09-16 15:39:05 +0200

    Added linked lanterns

---
+++ Assets/Scripts/LinkedLantern.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinkedLantern : Lantern
+{
+    public float WaitTime = 3f;
+    public Lantern MyLinkedLantern;
+
+    private float linkTime = 0f;
+    private bool linking = false;
+
+    private void Update() {
+        if (!linking)
+            return;
+
+        if(Time.time >= linkTime) {
+            LightLinkedLantern();
+        }
+    }
+
+    public override void Delight() {
+        base.Delight();
+
+        linking = false;
+    }
+
+    public override void Light() {
+        base.Light();
+
+        linking = true;
+        linkTime = Time.time + WaitTime;
+    }
+
+    private void LightLinkedLantern() {
+        if (MyLinkedLantern != null && Lit) {
+            Delight();
+            MyLinkedLantern.Light();
+        }
+    }
+}
